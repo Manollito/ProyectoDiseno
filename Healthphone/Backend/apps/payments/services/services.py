52642a51d58afdf820a5d83188693034f775ae59
KendallRC <99578801+KendallRC@users.noreply.github.com>
2024-10-20 12:09:01 -0600

    Delete Healthphone/Backend directory

--- Healthphone/Backend/apps/payments/services/services.py
+++
@@ -1,9 +0,0 @@
-from .factoryServices import PaymentFactory
-
-class PaymentService:
-    def process_payment(self, payment_method, amount):
-        # Obtén el procesador adecuado usando la Factory
-        processor = PaymentFactory.get_processor(payment_method)
-        
-        # Procesa el pago con el procesador obtenido
-        return processor.process_payment(amount)
