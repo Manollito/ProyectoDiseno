adf264bc320ed1d025d495e61f1649d430f5cd49
KendallRC <kendallrc28@gmail.com>
2024-11-03 21:48:05 -0600

    update

--- Healthphone/Backend/apps/scanner/views/categorization_view.py
+++ Healthphone/Backend/apps/scanner/views/categorization_view.py
@@ -21,6 +21,27 @@ class CategorizationView(APIView):
             )
     
     def post(self, request):
+        
+        response_data = {
+        "response": {
+            "result": "Leve",
+            "measures": {
+                "Temperatura": {
+                    "cantidad": 36.7,
+                    "unidad": "°C"
+                },
+                "Peso": {
+                    "cantidad": 70,
+                    "unidad": "kg"
+                },
+                "Altura": {
+                    "cantidad": 175,
+                    "unidad": "cm"
+                }
+            }
+        }
+    }
+        return JsonResponse(response_data)
        
         prompt = """Supone que estas escaneando a una persona, dime la temperatura, altura y peso. Responde solo en formato
             Temperatura: ...  Peso: ... Altura..."""
